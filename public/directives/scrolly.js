94603308bbe3b6ac369a3ddb9003aa3f04458513
Alan Ervey <alanrivera750@gmail.com>
2022-05-22 01:25:47 -0600

    Project deploy in production

---
+++ public/directives/scrolly.js
@@ -0,0 +1,21 @@
+AngularJsApp.directive('scrolly', function () {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var raw = element[0];
+
+            element.bind('scroll', function () {
+                if (scope.landscape){
+                    console.log(raw.scrollWidth + raw.offsetWidth > raw.scrollWidth);
+                    if (raw.scrollLeft + raw.offsetWidth > raw.scrollWidth) {
+                        scope.$apply(attrs.scrolly);
+                    }
+                }else{
+                    if (raw.scrollTop + raw.offsetHeight > raw.scrollHeight) {
+                        scope.$apply(attrs.scrolly);
+                    }
+                }
+            });
+        }
+    };
+});
